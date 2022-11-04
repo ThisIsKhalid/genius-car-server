451fafffcb5ae876ee7936bcfdbd644cbc2f1858
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-04 19:21:04 +0600

    CRUD --> R complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -20,11 +20,33 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection = client.db('geniusCar').collection('services');
+
+        // mongodb theke data anar jnno
+     //<------------ READ(R) ------------------------------------->   
+        app.get('/services', async (req,res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
+
 
 app.get("/", (req, res) => {
   res.send("Genius car is running !");
