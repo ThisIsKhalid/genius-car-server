9e04e4948fceaa03197e8e1ff74c4980ed0c0e00
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-05 09:12:11 +0600

    added delete api

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const client = new MongoClient(uri, {
 async function run(){
     try{
         const serviceCollection = client.db('geniusCar').collection('services');
+        const orderCollection = client.db('geniusCar').collection('orders');
 
         // mongodb theke data anar jnno
      //<------------ READ(R) ------------------------------------->   
@@ -39,6 +40,33 @@ async function run(){
             const service = await serviceCollection.findOne(query);
             res.send(service);
         })
+
+        // orders api
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+        // DELETE => (D)
+        app.delete('/orders/:id',async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally{
 
